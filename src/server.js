efd999bfec9b6cee851455c0acadc0f752f738f7
Kelvin Seibt <kelvin.seibt@gmail.com>
2023-05-02 13:47:54 -0300

    JSON body support

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import http from 'node:http'
 import { routes } from './routes.js'
+import { json } from './middlewares/json.js'
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
+    await json(req, res);
+
     console.log(`${method} REQUEST AT ${url}`)
+    console.log(req.body)
 
     const route = routes.find(route => {
         return route.method === method
